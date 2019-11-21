a10e5d14fe87f5e1c543c04ccc7ae77448782256
Sirine Dridi <sirinedridi@sirines-air.wireless-1x.unc.edu>
2019-11-21 15:56:28 -0500

    intents kinda

---
+++ app/src/main/java/com/example/financeapplication/Budget.java
@@ -0,0 +1,14 @@
+package com.example.financeapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Budget extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.budget_layout);
+    }
+
+}
