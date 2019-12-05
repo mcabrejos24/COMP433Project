be0e1ed3afc019582f83e85cde9bd2f796edd898
Sirine Dridi <sirine@live.unc.edu>
2019-12-05 17:26:28 -0500

    merge database and mic

--- app/src/main/java/com/example/financeapplication/FinanceLog.java
+++ app/src/main/java/com/example/financeapplication/FinanceLog.java
@@ -29,6 +29,23 @@ public class FinanceLog extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_layout);
         mDatabase = new DBHelper(this);
+        ImageView speak = findViewById(R.id.speak);
+        speak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Need to Speak");
+                try {
+                    startActivityForResult(intent, REQ_CODE);
+                } catch (ActivityNotFoundException a) {
+                    Toast.makeText(getApplicationContext(), "device not supported",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 
@@ -127,23 +144,7 @@ public class FinanceLog extends AppCompatActivity {
 
 
         }
-        ImageView speak = findViewById(R.id.speak);
-        speak.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Need to Speak");
-                try {
-                    startActivityForResult(intent, REQ_CODE);
-                } catch (ActivityNotFoundException a) {
-                    Toast.makeText(getApplicationContext(), "device not supported",
-                            Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
